a712b07f7d10324d253d162473fb1e15cbf3e537
Cameron Oldfield <cameron.oldfield@students.sacs.k12.in.us>
2016-12-20 09:47:37 -0500

    Changes to Winner
    Change the winner

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -379,9 +379,102 @@ def post_to_file(string, filename='tournament.txt', directory=''):
 ### Call main_play() if this file is executed
 if __name__ == '__main__':
     scores, moves, reports = main_play(modules[0:4])   
-<<<<<<< HEAD
     section0, section1, section2, section3 = reports
-### Somebody make it print something when score is over 1000
-=======
-    section0, section1, section2, section3 = reports
->>>>>>> parent of 3ff9dcc... Winning score
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+### Somebody make it print something when you win
